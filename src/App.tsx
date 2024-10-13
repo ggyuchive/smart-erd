9af7ec5dc61be640e0157cf365f1f9c5f63ffd34
ggyuchive <zarami1214@g.skku.edu>
2024-10-13 23:21:32 +0900

    [add] Simple login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login/Login';
 
-function App() {
+const App: React.FC = () => {
+  const handleLogin = (username: string, password: string) => {
+    console.log('Logging in with', { username, password });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login Page</h1>
+      <Login onLogin={handleLogin} />
     </div>
   );
-}
+};
 
 export default App;
